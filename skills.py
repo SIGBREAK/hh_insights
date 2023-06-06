3697384418271977dcdffb7c35f5b33b21489503
Roman A <AnanichRO@yandex.ru>
2023-06-06 18:44:49 +0300

    Fixed incorrect value of skills on chart. Refactoring.

--- skills.py
+++ skills.py
@@ -4,11 +4,10 @@ from parser import skills_list
 
 def write_skills(ws, n):
     ws.set_column('A:A', 30)
+    skills_data = reversed(Counter(skills_list).most_common(n))
 
-    row = 2
-    for data in reversed(Counter(skills_list).most_common(n)):
+    for row, data in enumerate(skills_data, 1):
         ws.write_row(f'A{row}', data)
-        row += 1
 
 
 def create_bar_chart(wb, chart_name, vacancy_name, n):
@@ -17,8 +16,8 @@ def create_bar_chart(wb, chart_name, vacancy_name, n):
     chartsheet.set_chart(chart)
 
     chart.add_series({'name': '=Навыки_табл!$B$1',
-                      'categories': f'=Навыки_табл!A2:A{n + 1}',
-                      'values': f'=Навыки_табл!B2:B{n + 1}'})
+                      'categories': f'=Навыки_табл!A1:A{n}',
+                      'values': f'=Навыки_табл!B1:B{n}'})
 
     chart.set_title({'name': f'Топ-{n} навыков: {vacancy_name}'})
     chart.set_x_axis({'name': 'Частота'})
