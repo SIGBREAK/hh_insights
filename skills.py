b9797143ecf7f81d432af3d52853bf9ac3e8b33d
Roman A <AnanichRO@yandex.ru>
2023-06-06 20:31:21 +0300

    Skills chart now looks good.

--- skills.py
+++ skills.py
@@ -17,9 +17,19 @@ def create_bar_chart(wb, chart_name, vacancy_name, n):
 
     chart.add_series({'name': '=Навыки_табл!$B$1',
                       'categories': f'=Навыки_табл!A1:A{n}',
-                      'values': f'=Навыки_табл!B1:B{n}'})
+                      'values': f'=Навыки_табл!B1:B{n}',
+                      'data_labels': {'value': True},
+                      'fill': {'color': 'blue'},
+                      'gradient': {'colors': ['#99CCFF', '#0066FF'], 'angle': 45}
+                      })
 
-    chart.set_title({'name': f'Топ-{n} навыков: {vacancy_name}'})
-    chart.set_x_axis({'name': 'Частота'})
+    chart.set_title({'name': f'Топ-{n} навыков: {vacancy_name}',
+                     'font': {'size': 15}})
 
-    chart.set_style(7)
+    chart.set_x_axis({'name': 'Частота',
+                      'name_font': {'size': 12, 'name': 'Times New Roman', 'bold': False},
+                      'num_format': '# ###', 'num_font': {'size': 12, 'name': 'Times New Roman'}})
+
+    chart.set_y_axis({'num_font': {'size': 12, 'name': 'Times New Roman'}})
+
+    chart.set_legend({'none': True})
